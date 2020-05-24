0f3860ca826ce3d23ee5b8d62a3cabdccdebeb00
Luke Barratt <luke.barratt@cpwonlinesolutions.com>
2020-05-24 17:59:00 +0100

    Adding user inputs

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,6 @@ class MyHomePage extends StatelessWidget {
         title: Text('Flutter App'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Container(
@@ -46,6 +45,27 @@ class MyHomePage extends StatelessWidget {
               child: Text('CHART!'),
               color: Colors.blue,
               elevation: 5,
+            ),
+          ),
+          Card(
+            elevation: 5,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Title'),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: "Amount"),
+                  ),
+                  FlatButton(
+                    child: Text('Add Transaction'),
+                    onPressed: () {},
+                  )
+                ],
+              ),
             ),
           ),
           Column(
